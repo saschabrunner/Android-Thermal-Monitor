e77e587a84b51ee9dfae3941770301ec46a6698f
Sascha Brunner <sascha.brunner@outlook.com>
2018-10-16 14:37:57 +0100

    Monitor frequencies of hard coded CPU cores

--- app/src/main/java/com/gitlab/saschabrunner/thermalmonitor/MainActivity.java
+++ app/src/main/java/com/gitlab/saschabrunner/thermalmonitor/MainActivity.java
@@ -14,33 +14,51 @@ import java.io.Reader;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    // Hard coded paths for OnePlus 3
+    private static final String TEMPERATURE_FILE_PATH =
+            "/sys/class/thermal/thermal_zone23/temp";
+    private static final String CPU0_FREQ_FILE_PATH =
+            "/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq";
+    private static final String CPU1_FREQ_FILE_PATH =
+            "/sys/devices/system/cpu/cpu1/cpufreq/scaling_cur_freq";
+    private static final String CPU2_FREQ_FILE_PATH =
+            "/sys/devices/system/cpu/cpu2/cpufreq/scaling_cur_freq";
+    private static final String CPU3_FREQ_FILE_PATH =
+            "/sys/devices/system/cpu/cpu3/cpufreq/scaling_cur_freq";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView tvTemperature = findViewById(R.id.tvTemperature);
         final TextView tvFilePath = findViewById(R.id.tvFilePath);
-        final TextView tvTime = findViewById(R.id.tvTime);
-
-        // Hard coded path for sensor 'msm_therm' on OnePlus 3
-        final String filePath = "/sys/class/thermal/thermal_zone23/temp";
-        tvFilePath.setText(filePath);
+        tvFilePath.setText(TEMPERATURE_FILE_PATH);
 
         Thread monitoringThread = new Thread(new Runnable() {
             @Override
             public void run() {
+                final TextView tvTemperature = findViewById(R.id.tvTemperature);
+                final TextView tvTime = findViewById(R.id.tvTime);
+                final TextView tvCpu0Freq = findViewById(R.id.tvCpu0Freq);
+                final TextView tvCpu1Freq = findViewById(R.id.tvCpu1Freq);
+                final TextView tvCpu2Freq = findViewById(R.id.tvCpu2Freq);
+                final TextView tvCpu3Freq = findViewById(R.id.tvCpu3Freq);
+
                 while (true) {
                     // Can't touch views from separate thread
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            tvTemperature.setText(readFile(filePath));
+                            tvTemperature.setText(readFile(TEMPERATURE_FILE_PATH));
+                            tvCpu0Freq.setText(readFile(CPU0_FREQ_FILE_PATH));
+                            tvCpu1Freq.setText(readFile(CPU1_FREQ_FILE_PATH));
+                            tvCpu2Freq.setText(readFile(CPU2_FREQ_FILE_PATH));
+                            tvCpu3Freq.setText(readFile(CPU3_FREQ_FILE_PATH));
                             tvTime.setText(new Date().toString());
                         }
                     });
                     try {
-                        Thread.sleep(1000);
+                        Thread.sleep(500);
                     } catch (InterruptedException e) {
                         return;
                     }
