832fecf20931280bc70e95a2d01423614d59e9a9
Sascha Brunner <sascha.brunner@outlook.com>
2018-12-08 16:17:04 +0000

    Show offline state again

--- app/src/main/java/com/gitlab/saschabrunner/thermalmonitor/CPUFreqMonitor.java
+++ app/src/main/java/com/gitlab/saschabrunner/thermalmonitor/CPUFreqMonitor.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 public class CPUFreqMonitor implements Runnable, Monitor {
@@ -47,7 +48,7 @@ public class CPUFreqMonitor implements Runnable, Monitor {
             for (CPU cpu : cpus) {
                 OverlayListItem listItem = listItemByCpu.get(cpu);
                 assert listItem != null;
-                listItem.setValue(cpu.getLastFrequency() + "KHz");
+                listItem.setValue(buildCpuValueString(cpu));
                 monitorService.updateListItem(listItem);
             }
 
@@ -71,6 +72,15 @@ public class CPUFreqMonitor implements Runnable, Monitor {
                 e.printStackTrace();
                 return;
             }
+        }
+    }
+
+    private String buildCpuValueString(CPU cpu) {
+        if (cpu.getLastState() == CPU.STATE_OFFLINE) {
+            return monitorService.getString(R.string.offline);
+        } else {
+            return String.format(Locale.getDefault(), "%6.1f MHz",
+                    (double) cpu.getLastFrequency() / 1000);
         }
     }
 }
