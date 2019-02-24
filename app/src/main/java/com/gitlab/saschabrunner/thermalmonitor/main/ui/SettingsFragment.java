64c73b58bd10bcaebb627e7c6044558917d4f037
Sascha Brunner <sascha.brunner@outlook.com>
2019-02-24 15:36:07 +0000

    Check if root access is allowed when enabling it

--- app/src/main/java/com/gitlab/saschabrunner/thermalmonitor/main/ui/SettingsFragment.java
+++ app/src/main/java/com/gitlab/saschabrunner/thermalmonitor/main/ui/SettingsFragment.java
@@ -7,10 +7,13 @@ import android.os.Bundle;
 import android.provider.Settings;
 
 import com.gitlab.saschabrunner.thermalmonitor.R;
+import com.gitlab.saschabrunner.thermalmonitor.util.MessageUtils;
+import com.topjohnwu.superuser.Shell;
 
 import java.util.Objects;
 
 import androidx.annotation.RequiresApi;
+import androidx.preference.CheckBoxPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
@@ -24,6 +27,9 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.fragment_settings, rootKey);
 
+        CheckBoxPreference rootEnabled = findPreference("rootEnabled");
+        rootEnabled.setOnPreferenceChangeListener(this::onRootEnabledChanged);
+
         Preference overlaySettings = findPreference("dummyOverlaySettings");
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             overlaySettings.setVisible(true);
@@ -36,6 +42,19 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
                 Uri.parse("package:" + Objects.requireNonNull(getContext()).getPackageName()));
         startActivity(intent);
+        return true;
+    }
+
+    private boolean onRootEnabledChanged(Preference preference, Object newValue) {
+        boolean rootEnabled = (boolean) newValue;
+
+        // Check if root access is allowed
+        if (rootEnabled && !Shell.rootAccess()) {
+            MessageUtils.showInfoDialog(getContext(), R.string.rootAccessDenied,
+                    R.string.couldNotAcquireRootAccess);
+            return false;
+        }
+
         return true;
     }
 }
